65a75791be3a2263df532effaa981858f33176b5
manju <kudukacar@hotmail.com>
2019-11-05 16:02:20 -0500

    Updates

---
+++ precache-manifest.1ffe125f4398f9ff5a940299a5654c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0522422ca4f92df1e2b68fcb85dbcf0",
+    "url": "/smart/index.html"
+  },
+  {
+    "revision": "832a11253f5b8af8ef33",
+    "url": "/smart/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "38c871f598126fb2357d",
+    "url": "/smart/static/js/2.3db055fd.chunk.js"
+  },
+  {
+    "revision": "832a11253f5b8af8ef33",
+    "url": "/smart/static/js/main.8f00de6e.chunk.js"
+  },
+  {
+    "revision": "a13c6c3f4a9ad1c295ea",
+    "url": "/smart/static/js/runtime-main.f823baae.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/smart/static/media/logo.25bf045c.svg"
+  }
+]);
