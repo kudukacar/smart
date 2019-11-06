8075c57c407db4161b0673d80b8c774734c43961
manju <kudukacar@hotmail.com>
2019-11-05 19:46:08 -0500

    create navbar

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>mthomas@smartmovesphysicaltherapy.com</div>
+      <div>678-372-7972</div>
+      <div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from '../images/logo.png';
+
+const NavBar = () => {
+  return(
+    <header className="nav">
+      <nav className="leftnav">
+        <img src={logo} alt="logo" />
+        <div>
+          Smart Moves Physical Therapy
+        </div>
+      </nav>
+      <nav className="rightnav">
+        <div>About</div>
+        <div>Services</div>
+        <div>Insurance</div>
+        <div>Contact</div>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar;
